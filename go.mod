055860a04fe1fa311f3847d8289073a07e34981f
Paxx <daniel.botta@hiway.media>
2023-11-15 18:07:16 +0100

    feat: improved the pet interface

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module virtualpet
 
-go 1.20
+go 1.21
 
 require github.com/gdamore/tcell/v2 v2.6.0
 
--- painter/service.go
+++ painter/service.go
@@ -2,6 +2,7 @@ package painter
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 	"virtualpet/pet"
@@ -28,6 +29,7 @@ func NewUi(screen tcell.Screen, command *string, pet pet.IPet) IUIService {
 }
 
 func (u *uiService) Start() {
+	go u.updateGame()
 	for {
 		u.screen.Clear()
 		u.functionMutex.RLock()
@@ -42,38 +44,69 @@ func (u *uiService) Start() {
 	}
 }
 
+func (u *uiService) updateGame() {
+	c := time.NewTicker(time.Second)
+
+	for {
+		select {
+		case <-c.C:
+			if rand.Intn(100) < 5 {
+				u.pet.Starve(rand.Int31n(10))
+			}
+			if rand.Intn(100) < 5 {
+				u.pet.HappinessDecay(rand.Int31n(10))
+			}
+			if rand.Intn(100) < 1 {
+				u.pet.Shit()
+			}
+			u.pet.Die()
+		default:
+		}
+	}
+}
+
 func (u *uiService) drawGame() {
+	if u.pet.GetPet().Dead {
+		u.drawGameOver()
+		return
+	}
 	u.drawHeader(20)
 	u.drawStats(0, 0, 20, 20)
 	u.drawPet(22, 2)
+	u.drawShit(44, 2)
 }
 
 func (u *uiService) drawStats(x, y, width, height int) {
 	getPet := u.pet.GetPet()
 	u.drawBox(x, y, width, height)
-	u.drawText(x+1, y+1, fmt.Sprintf("Name: %s", getPet.Name))
-	u.drawText(x+1, y+2, fmt.Sprintf("Type: %s", getPet.Type))
-	u.drawText(x+1, y+3, fmt.Sprintf("Hunger: %d", getPet.Hunger))
-	u.drawText(x+1, y+4, fmt.Sprintf("Happiness: %d", getPet.Happiness))
-	u.drawText(x+1, y+5, fmt.Sprintf("Location: %s", getPet.Location))
+	u.drawText(x+1, y+1, fmt.Sprintf("%s | %s", getPet.Name, getPet.Type))
+	u.drawText(x+1, y+2, fmt.Sprintf("📍 %s", getPet.Location))
+	u.drawText(x+1, y+3, "🍕")
+	u.drawProgressBar(x+5, y+3, getPet.Satisfaction, pet.MaxStat)
+	u.drawText(x+1, y+4, "🎭")
+	u.drawProgressBar(x+5, y+4, getPet.Happiness, pet.MaxStat)
 }
 
 func (u *uiService) drawPet(x, y int) {
-	startX := x
 	animationFrames = u.pet.GetFrames()
 	if u.pet == nil {
 		return
 	}
-	for _, t := range []rune(animationFrames[animationIndex]) {
+	u.drawShape(x, y, []rune(animationFrames[animationIndex]))
+	if u.frameCounter%60 == 0 {
+		animationIndex = (animationIndex + 1) % len(animationFrames)
+	}
+}
+
+func (u *uiService) drawShape(x int, y int, shape []rune) {
+	startX := x
+	for _, t := range shape {
 		if t == '\n' {
 			x = startX
 			y += 1
 		}
 		u.screen.SetContent(x, y, t, nil, defaultStyle)
 		x += 1
-	}
-	if u.frameCounter%60 == 0 {
-		animationIndex = (animationIndex + 1) % len(animationFrames)
 	}
 }
 
@@ -82,4 +115,23 @@ func (u *uiService) drawHeader(x int) {
 	for i, t := range []rune(header + *u.command) {
 		u.screen.SetContent(i+x, 0, t, nil, defaultStyle)
 	}
+}
+
+func (u *uiService) drawGameOver() {
+	u.drawText(0, 0, fmt.Sprintf("%s is dead!", u.pet.GetPet().Name))
+}
+
+func (u *uiService) drawShit(x, y int) {
+	const shitASCII = `     (   )
+  (   ) (
+   ) _   )
+    ( \_
+  _(_\ \)__
+ (____\___))   
+	`
+
+	if u.pet.GetPet().ShitView <= 0 {
+		return
+	}
+	u.drawShape(x, y, []rune(shitASCII))
 }
